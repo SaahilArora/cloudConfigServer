311eb5967eca13fc3567888946b591f672acf6df
Sahil Arora <arorasaahil1994@gmail.com>
2022-12-17 17:05:13 +0530

    Dev and QA Added

--- src/main/java/com/example/cloudconfigserver/CloudconfigserverApplication.java
+++ src/main/java/com/example/cloudconfigserver/CloudconfigserverApplication.java
@@ -3,9 +3,11 @@ package com.example.cloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableConfigServer
 @SpringBootApplication
+@RefreshScope
 public class CloudconfigserverApplication {
 
 	public static void main(String[] args) {
